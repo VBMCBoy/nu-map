54fdb7bcbfed18a6d47f2e41498b2d19fd03b074
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2023-08-19 13:13:09 +0200

    implement Bluetooth Cypress device

--- numap/apps/base.py
+++ numap/apps/base.py
@@ -22,6 +22,7 @@ class NumapApp(object):
         self.umap_class_dict = {
             'audio': ('audio', 'Headset'),
             'billboard': ('billboard', 'A billboard, requires USB-C'),
+            'bluetooth_cypress': ('bluetooth_cypress', 'A Bluetooth Cypress adapter.'),
             'cdc_acm': ('cdc_acm', 'Abstract Control Model device (like serial modem)'),
             'cdc_dl': ('cdc_dl', 'Direct Line Control device (like modem)'),
             'ftdi': ('ftdi', 'USB<->RS232 FTDI chip'),
--- numap/dev/bluetooth_cypress.py
+++ numap/dev/bluetooth_cypress.py
@@ -1,7 +1,7 @@
 '''
-Contains class definitions to implement an RNDIS device, i.e. an Ethernet adapter based on Microsoft's standard.
+Contains class definitions to implement a Bluetooth Cypress device, i.e. a Bluetooth adapter.
 
-Implemented as per https://learn.microsoft.com/en-us/windows-hardware/drivers/network/usb-802-3-device-sample
+Implemented as per the lsusb output of a 04b4:f901 Cypress Semiconductor Corp. CYW20704A2.
 '''
 
 from numap.core.usb_class import USBClass
@@ -10,29 +10,75 @@ from numap.core.usb_configuration import USBConfiguration
 from numap.core.usb_interface import USBInterface
 from numap.core.usb_endpoint import USBEndpoint
 from numap.fuzz.helpers import mutable
-class USBRndisClass(USBClass):
-    name = 'RndisClass'
+
+
+class USBBluetoothCypressClass(USBClass):
+    name = 'BluetoothCypressClass'
 
     def setup_local_handlers(self):
         self.local_handlers = {
-            0x20: self.handle_unknown,
-            0x21: self.handle_unknown,
-            0x22: self.handle_unknown,
+            x: self.handle_unknown for x in range(0, 256)
         }
 
     @mutable('handle_unknown')
     def handle_unknown(self, req):
-        # in theory, this should act like
-        # https://learn.microsoft.com/en-us/windows-hardware/drivers/network/control-channel-characteristics
-        # however, since the (Windows)-PC calls this function, we may already know that the host supports this device
-
-        # self.info(req)
         return b''
 
 
+class DataInterface(USBInterface):
+    name = 'DataInterface'
 
-class CCInterface(USBInterface):
-    name = 'CCInterface'
+    def __init__(self, app, phy, alternate_setting, max_packet_size):
+        endpoints = [
+            USBEndpoint(
+                app=app,
+                phy=phy,
+                number=3,
+                direction=USBEndpoint.direction_in,
+                transfer_type=USBEndpoint.transfer_type_isochronous,
+                sync_type=USBEndpoint.sync_type_none,
+                usage_type=USBEndpoint.usage_type_data,
+                max_packet_size=max_packet_size,
+                interval=1,
+                handler=self.handle_data_available,
+                usb_class=USBBluetoothCypressClass(app, phy)
+        ),
+            USBEndpoint(
+                app=app,
+                phy=phy,
+                number=3,
+                direction=USBEndpoint.direction_out,
+                transfer_type=USBEndpoint.transfer_type_isochronous,
+                sync_type=USBEndpoint.sync_type_none,
+                usage_type=USBEndpoint.usage_type_data,
+                max_packet_size=max_packet_size,
+                interval=1,
+                handler=None,
+                usb_class=USBBluetoothCypressClass(app, phy)
+            )
+
+        ]
+
+        super().__init__(
+            app=app,
+            phy=phy,
+            interface_number=1,
+            interface_alternate=alternate_setting,
+            interface_class=0xe0,  # Wireless
+            interface_subclass=0x01,  # Radio Frequency
+            interface_protocol=0x01,  # Bluetooth
+            interface_string_index=0,
+            endpoints=endpoints,
+            usb_class=USBBluetoothCypressClass(app, phy)
+        )
+
+    @mutable('handle_data_available')
+    def handle_data_available(self, data):
+        self.info(data)
+
+
+class SuperDataInterface(USBInterface):
+    name = 'SuperDataInterface'
 
     def __init__(self, app, phy):
         endpoints = [
@@ -41,38 +87,14 @@ class CCInterface(USBInterface):
                 phy=phy,
                 number=1,
                 direction=USBEndpoint.direction_in,
-                transfer_type=USBEndpoint.transfer_type_bulk,
+                transfer_type=USBEndpoint.transfer_type_interrupt,
                 sync_type=USBEndpoint.sync_type_none,
                 usage_type=USBEndpoint.usage_type_data,
-                max_packet_size=0x0008,
-                interval=0x01,
-                handler=self.handle_data_available
-            )
-        ]
-
-        super().__init__(
-            app=app,
-            phy=phy,
-            interface_number=0,
-            interface_alternate=0,
-            interface_class=0x02,
-            interface_subclass=0x02,
-            interface_protocol=0xff,
-            interface_string_index=0,
-            endpoints=endpoints,
-            usb_class=USBRndisClass(app, phy)
-        )
-
-    @mutable('handle_data_available')
-    def handle_data_available(self, data):
-        self.info(data)
-
-
-class DCInterface(USBInterface):
-    name = 'DCInterface'
-
-    def __init__(self, app, phy):
-        endpoints = [
+                max_packet_size=0x0010,
+                interval=1,
+                handler=self.handle_data_available,
+                usb_class=USBBluetoothCypressClass(app, phy)
+            ),
             USBEndpoint(
                 app=app,
                 phy=phy,
@@ -82,35 +104,37 @@ class DCInterface(USBInterface):
                 sync_type=USBEndpoint.sync_type_none,
                 usage_type=USBEndpoint.usage_type_data,
                 max_packet_size=0x0040,
-                interval=0,
-                handler=self.handle_data_available
-            ),
+                interval=1,
+                handler=self.handle_data_available,
+                usb_class=USBBluetoothCypressClass(app, phy)
+        ),
             USBEndpoint(
                 app=app,
                 phy=phy,
-                number=3,
+                number=2,
                 direction=USBEndpoint.direction_out,
                 transfer_type=USBEndpoint.transfer_type_bulk,
                 sync_type=USBEndpoint.sync_type_none,
                 usage_type=USBEndpoint.usage_type_data,
                 max_packet_size=0x0040,
-                interval=0,
-                handler=None
-            )
+                interval=1,
+                handler=None,
+                usb_class=USBBluetoothCypressClass(app, phy)
 
+            )
         ]
 
         super().__init__(
             app=app,
             phy=phy,
-            interface_number=1,
+            interface_number=0,
             interface_alternate=0,
-            interface_class=0x0a,
-            interface_subclass=0x00,
-            interface_protocol=0x00,
+            interface_class=0xe0,  # Wireless
+            interface_subclass=0x01,  # Radio Frequency
+            interface_protocol=0x01,  # Bluetooth
             interface_string_index=0,
             endpoints=endpoints,
-            usb_class=USBRndisClass(app, phy)
+            usb_class=USBBluetoothCypressClass(app, phy)
         )
 
     @mutable('handle_data_available')
@@ -118,36 +142,96 @@ class DCInterface(USBInterface):
         self.info(data)
 
 
-class USBRndisDevice(USBDevice):
-    name = 'RndisDevice'
+class VendorSpecificInterface(USBInterface):
+    name = 'VendorSpecificInterface'
+
+    def __init__(self, app, phy):
+        endpoints = [
+            USBEndpoint(
+                app=app,
+                phy=phy,
+                number=4,
+                direction=USBEndpoint.direction_in,
+                transfer_type=USBEndpoint.transfer_type_bulk,
+                sync_type=USBEndpoint.sync_type_none,
+                usage_type=USBEndpoint.usage_type_data,
+                max_packet_size=0x0020,
+                interval=1,
+                handler=self.handle_data_available,
+                usb_class=USBBluetoothCypressClass(app, phy)
+            ),
+            USBEndpoint(
+                app=app,
+                phy=phy,
+                number=4,
+                direction=USBEndpoint.direction_out,
+                transfer_type=USBEndpoint.transfer_type_bulk,
+                sync_type=USBEndpoint.sync_type_none,
+                usage_type=USBEndpoint.usage_type_data,
+                max_packet_size=0x0020,
+                interval=1,
+                handler=self.handle_data_available,
+                usb_class=USBBluetoothCypressClass(app, phy)
+            )
+        ]
+
+        super().__init__(
+            app=app,
+            phy=phy,
+            interface_number=2,
+            interface_alternate=0,
+            interface_class=0xff,  # Vendor Specific
+            interface_subclass=0xff,  # Vendor Specific
+            interface_protocol=0xff,  # Vendor Specific
+            interface_string_index=0,
+            endpoints=endpoints,
+            usb_class=USBBluetoothCypressClass(app, phy)
+        )
+
+    @mutable('handle_data_available')
+    def handle_data_available(self, data):
+        self.info(data)
+
+
+# The device additionally contains a DFU Interface, but I believe we can't emulate this (yet?)
+
+class USBBluetoothCypressDevice(USBDevice):
+    name = 'BluetoothCypressDevice'
 
     def __init__(self, app, phy):
         super().__init__(
             app=app,
             phy=phy,
-            device_class=USBClass.CDC,
-            device_subclass=0,
-            protocol_rel_num=0,
+            device_class=USBClass.WirelessController,
+            device_subclass=1,  # Radio Frequency
+            protocol_rel_num=1,  # Bluetooth
             max_packet_size_ep0=64,
-            vendor_id=0x2001,  # D-Link Corp.
-            product_id=0x4a00,  # DUB-1312 Gigabit Ethernet Adapter
+            vendor_id=0x04b4,  # Cypress Semiconductor Corp.
+            product_id=0xf901,
             device_rev=1,
-            manufacturer_string='numap Inc.',
-            product_string='numap RNDIS Network Interface',
-            serial_number_string='0123456789-1337',
+            manufacturer_string='Cypress Semi',
+            product_string='CYW20704A2',
+            serial_number_string='0016A44CB785',
             configurations=[
                 USBConfiguration(
                     app=app,
                     phy=phy,
                     index=1,
-                    string='RNDIS',
+                    string='',
                     interfaces=[
-                        CCInterface(app, phy),
-                        DCInterface(app, phy)
-                    ]
+                        SuperDataInterface(app, phy),
+                        DataInterface(app, phy, 0, 0),
+                        DataInterface(app, phy, 1, 9),
+                        DataInterface(app, phy, 2, 0x11),
+                        DataInterface(app, phy, 3, 0x19),
+                        DataInterface(app, phy, 4, 0x21),
+                        DataInterface(app, phy, 5, 0x31),
+                        VendorSpecificInterface(app, phy)
+                    ],
                 )
-            ]
+            ],
+            usb_class=USBBluetoothCypressClass(app, phy)
         )
 
 
-usb_device = USBRndisDevice
+usb_device = USBBluetoothCypressDevice
